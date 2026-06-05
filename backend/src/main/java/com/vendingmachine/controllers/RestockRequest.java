a5177bc588b1c401f098878460203385f46b555c
haiqakhalil28@gmail.com <haiqakhalil28@email.com>
2026-06-05 06:58:37 +0500

    RestockRequest class

--- backend/src/main/java/com/vendingmachine/controllers/RestockRequest.java
+++ backend/src/main/java/com/vendingmachine/controllers/RestockRequest.java
@@ -1,9 +1,15 @@
 package com.vendingmachine.controllers;
 
 public class RestockRequest {
+
     private int itemId;
     private int quantityAdded;
 
-    public int getItemId() { return itemId; }
-    public int getQuantityAdded() { return quantityAdded; }
+    public int getItemId() {
+        return itemId;
+    }
+
+    public int getQuantityAdded() {
+        return quantityAdded;
+    }
 }
