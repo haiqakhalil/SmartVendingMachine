ff56ff820e0d4c918ebedf629267bfe2d5441c30
haiqakhalil28@gmail.com <haiqakhalil28@email.com>
2026-06-05 06:57:23 +0500

    BuyRequest class

--- backend/src/main/java/com/vendingmachine/controllers/BuyRequest.java
+++ backend/src/main/java/com/vendingmachine/controllers/BuyRequest.java
@@ -1,11 +1,20 @@
 package com.vendingmachine.controllers;
 
 public class BuyRequest {
+
     private int itemId;
     private String paymentType;
     private double amountGiven;
 
-    public int getItemId() { return itemId; }
-    public String getPaymentType() { return paymentType; }
-    public double getAmountGiven() { return amountGiven; }
+    public int getItemId() {
+        return itemId;
+    }
+
+    public String getPaymentType() {
+        return paymentType;
+    }
+
+    public double getAmountGiven() {
+        return amountGiven;
+    }
 }
