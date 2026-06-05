97917ca90b2b9d52c82d6cd0585eff34a537eddb
haiqakhalil28@gmail.com <haiqakhalil28@email.com>
2026-06-05 05:47:00 +0500

    OperatorRepository interface

--- backend/src/main/java/com/vendingmachine/models/OperatorRepository.java
+++ backend/src/main/java/com/vendingmachine/models/OperatorRepository.java
@@ -1,10 +1,9 @@
 package com.vendingmachine.models;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
-@Repository
 public interface OperatorRepository extends JpaRepository<Operator, Integer> {
+
     Optional<Operator> findByUsername(String username);
 }
